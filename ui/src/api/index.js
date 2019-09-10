9b52841857469792e3675a6146734e383c228831
Sean Nakamura <seanenakamura@gmail.com>
2019-09-10 16:32:27 -0700

    Working getLogs

--- ui/src/api/index.js
+++ ui/src/api/index.js
@@ -2,18 +2,23 @@ const API_PREFIX = '/api';
 
 const apiCall = (uri, headers = {}) => fetch(API_PREFIX + uri, {
   headers,
-}).then(response => response.json());
+});
 
 // listPrefixes /list-prefixes
-export const listPrefixes = () => apiCall('/list-prefixes');
+export const listPrefixes = () => apiCall('/list-prefixes').then(response => response.json());
 
 // listKeys /list-keys?prefix=''
-export const listPrefixKeys = prefix => apiCall(`/list-keys?prefix=${prefix}`);
+export const listPrefixKeys = prefix => apiCall(`/list-keys?prefix=${prefix}`)
+  .then(response => response.json());
 
 // getLogsStream /get-logs-stream?prefix=''
-export const getLogsStream = prefix => apiCall(`/get-logs-stream?prefix=${prefix}`);
+export const getLogsStream = prefix => apiCall(`/get-logs-stream?prefix=${prefix}`)
+  .then(response => response.text())
+  .then(data => data.split('\n'));
 
 // tailLogsStream /tail-logs-stream
-export const tailLogsStream = prefix => apiCall(`/get-logs-stream?prefix=${prefix}`, {
-  'Content-Type': 'application/x-ndjson',
-});
+// export const tailLogsStream = prefix => apiCall(`/get-logs-stream?prefix=${prefix}`, {
+//   'Content-Type': 'application/x-ndjson',
+// })
+//   .then(response => response.text())
+//   .then(data => data);
--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -5,12 +5,13 @@ import { Box, Heading, Button } from 'rebass';
 import {
   listPrefixes,
   listPrefixKeys,
+  getLogsStream,
 } from '../api';
 
 const initialState = {
   prefixes: [],
   selectedPrefix: '',
-  output: '',
+  output: [],
 }
 
 const App = () => {
@@ -28,7 +29,13 @@ const App = () => {
   });
 
   const getKeysHandler = () => listPrefixKeys(state.selectedPrefix)
-    .then((response) => setState({ ...state, output: JSON.stringify(response) }))
+    .then(({ keys }) => setState({
+      ...state,
+      output: keys,
+    }))
+
+  const getLogsHandler = () => getLogsStream(state.selectedPrefix)
+    .then((response) => setState({ ...state, output: response }))
 
   return (
     <Box>
@@ -50,15 +57,17 @@ const App = () => {
         {state.selectedPrefix && (
           <Fragment>
             <Button color='black' onClick={getKeysHandler}>Get Keys</Button>
-            <Button>Get Logs</Button>
-            <Button>Tail Logs</Button>
+            <Button color='black' onClick={getLogsHandler}>Get Logs</Button>
+            <Button color='black' onClick={() => {}}>Tail Logs</Button>
           </Fragment>
         )}
       </Box>
       <Box>
         <Heading fontSize={5}>Output</Heading>
         <Box pad={5}>
-          {state.output}
+          {state.output && state.output.map(item => (
+            <li key={item}>{item}</li>
+          ))}
         </Box>
       </Box>
     </Box>
