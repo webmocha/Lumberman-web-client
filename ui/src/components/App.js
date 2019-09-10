acce745d510446ffcb546c58a3cf6510d5ecf09d
Sean Nakamura <seanenakamura@gmail.com>
2019-09-10 14:57:20 -0700

    Initial listing of prefixes

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Box, Heading } from 'rebass';
 
-const App = () => (
-  <Box>
-    <Heading>Hello World</Heading>
-  </Box>
-);
+import { listPrefixes } from '../api';
+
+const initialState = {
+  prefixes: [],
+  key: '',
+}
+
+const App = () => {
+  const [state, setState] = useState(initialState);
+
+  React.useEffect(() => {
+    listPrefixes().then(({ prefixes }) => setState({
+      prefixes,
+    }))
+  }, []);
+
+  return (
+    <Box>
+      <Box>
+        <Heading fontSize={5}>Prefixes</Heading>
+        <ul>
+          {state.prefixes.map(prefix => <li key={prefix}>{prefix}</li>)}
+        </ul>
+      </Box>
+    </Box>
+  )
+};
 
 export default App;
