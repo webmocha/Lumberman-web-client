942650d51ea70ca5a1bc15ac3b6bc6a71af7cb87
Sean Nakamura <seanenakamura@gmail.com>
2019-09-10 15:32:58 -0700

    Update apiCalls and add tailLogsStream

--- ui/src/api/index.js
+++ ui/src/api/index.js
@@ -1,12 +1,19 @@
 const API_PREFIX = '/api';
 
-const apiCall = uri => fetch(API_PREFIX + uri).then(response => response.json());
+const apiCall = (uri, headers = {}) => fetch(API_PREFIX + uri, {
+  headers,
+}).then(response => response.json());
 
 // listPrefixes /list-prefixes
 export const listPrefixes = () => apiCall('/list-prefixes');
 
 // listKeys /list-keys?prefix=''
-export const listPrefixKeys = prefix => apiCall(`/list-keys?=${prefix}`);
+export const listPrefixKeys = prefix => apiCall(`/list-keys?prefix=${prefix}`);
 
 // getLogsStream /get-logs-stream?prefix=''
-export const getLogsStream = prefix => apiCall(`/get-logs-stream?=${prefix}`);
+export const getLogsStream = prefix => apiCall(`/get-logs-stream?prefix=${prefix}`);
+
+// tailLogsStream /tail-logs-stream
+export const tailLogsStream = prefix => apiCall(`/get-logs-stream?prefix=${prefix}`, {
+  'Content-Type': 'application/x-ndjson',
+});
